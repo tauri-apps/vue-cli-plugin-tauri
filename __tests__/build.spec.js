f77d03fc1d8726585b770b8db1077031dff64671
Lucas Nogueira <lucas@tauri.studio>
2022-05-23 09:09:57 -0300

    chore(deps): update @tauri-apps/cli to rc.11, closes #71

--- __tests__/build.spec.js
+++ __tests__/build.spec.js
@@ -1,9 +1,16 @@
+const path = require('path')
+const fs = require('fs')
+
 jest.setTimeout(240000)
 const create = require('./createProject.helper')
 
 describe('tauri:build', () => {
   it('Should build a tauri app', async () => {
     const project = await create('build')
+    const configPath = path.resolve(project.dir, 'src-tauri/tauri.conf.json')
+    const config = fs.readFileSync(configPath).toString()
+    fs.writeFileSync(configPath, config.replace('com.tauri.dev', 'com.tauri.test'))
+
     await project.run('vue-cli-service tauri:build')
     // Web code is built
     expect(project.has('src-tauri/target/webpack_dist/index.html')).toBe(true)
